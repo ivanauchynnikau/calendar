640b3cfe7d63f069870bcccfd658485d98dfce36
i.ovchinnikov <i.ovchinnikov@lovata.com>
2018-10-29 11:15:54 +0300

    fix events list data

--- EVENTS.js
+++ EVENTS.js
@@ -1,58 +1,38 @@
 export default [
-  {
-    "name": "October 23 00:00-06:30",
-    "startDate": "2018-10-23T00:00:00+03:00",
-    "endDate": "2018-10-23T06:29:59+03:00",
+   {
+    "name": "October 29 01:00-06:30",
+    "startDate": "2018-10-29T01:00:00+03:00",
+    "endDate": "2018-10-29T06:29:59+03:00",
     "description": "Some text that describe event details etc.."
   }, {
-    "name": "October 22 01:00-06:30",
-    "startDate": "2018-10-22T01:00:00+03:00",
-    "endDate": "2018-10-22T06:29:59+03:00",
-    "description": "Some text that describe event details etc.."
-  },{
-    "name": "October 22 01:00-02:00",
-    "startDate": "2018-10-22T01:00:00+03:00",
-    "endDate": "2018-10-22T01:59:59+03:00",
-    "description": "Some text that describe event details etc.."
-  },{
-    "name": "October 22 01:00-02:00",
-    "startDate": "2018-10-22T01:00:00+03:00",
-    "endDate": "2018-10-22T01:59:59+03:00",
+    "name": "October 29 01:00-02:00",
+    "startDate": "2018-10-29T01:00:00+03:00",
+    "endDate": "2018-10-29T01:59:59+03:00",
     "description": "Some text that describe event details etc.."
   }, {
-    "name": "October 23 18:00-20:00",
-    "startDate": "2018-10-23T18:00:00+03:00",
-    "endDate": "2018-10-23T19:59:59+03:00",
+    "name": "October 31 18:00-20:00",
+    "startDate": "2018-10-31T18:00:00+03:00",
+    "endDate": "2018-10-31T19:59:59+03:00",
     "description": "Some text that describe event details etc.."
   }, {
-    "name": "October 23 18:00-21:30",
-    "startDate": "2018-10-23T18:00:00+03:000",
-    "endDate": "2018-10-23T21:29:59+03:00",
+    "name": "October 31 00:00-23:59",
+    "startDate": "2018-10-31T00:00:00+03:00",
+    "endDate": "2018-10-31T23:59:59+03:00",
     "description": "Some text that describe event details etc.."
   }, {
-    "name": "October 24 10:00-23:30",
-    "startDate": "2018-10-24T10:00:00+03:00",
-    "endDate": "2018-10-24T23:29:59+03:00",
+    "name": "October 29 11:00-12:00",
+    "startDate": "2018-10-29T11:00:00+03:00",
+    "endDate": "2018-10-29T11:59:59+03:00",
     "description": "Some text that describe event details etc.."
   }, {
-    "name": "October 25 00:00-23:59",
-    "startDate": "2018-10-25T00:00:00+03:00",
-    "endDate": "2018-10-25T23:59:59+03:00",
+    "name": "November 29 11:00-13:00",
+    "startDate": "2018-11-29T11:00:00+03:00",
+    "endDate": "2018-11-29T12:59:59+03:00",
     "description": "Some text that describe event details etc.."
   }, {
-    "name": "October 26 11:00-12:00",
-    "startDate": "2018-10-26T11:00:00+03:00",
-    "endDate": "2018-10-26T11:59:59+03:00",
-    "description": "Some text that describe event details etc.."
-  }, {
-    "name": "November 26 11:00-13:00",
-    "startDate": "2018-11-26T11:00:00+03:00",
-    "endDate": "2018-11-26T12:59:59+03:00",
-    "description": "Some text that describe event details etc.."
-  }, {
-    "name": "October 26 11:30-16:30",
-    "startDate": "2018-10-26T11:30:00+03:00",
-    "endDate": "2018-10-26T16:29:29+03:00",
+    "name": "October 30 11:30-16:30",
+    "startDate": "2018-10-30T11:30:00+03:00",
+    "endDate": "2018-10-30T16:29:29+03:00",
     "description": "Some text that describe event details etc.."
   }
 ];
