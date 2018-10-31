459e1828832e22cae6e2674809202f18ed6e2d3d
i.ovchinnikov <i.ovchinnikov@lovata.com>
2018-10-31 19:57:07 +0300

    connect react dev tools

--- src/components/store.js
+++ src/components/store.js
@@ -2,10 +2,14 @@ import { createStore, combineReducers } from 'redux';
 
 import { calendar } from './reducers';
 
+const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__;
 const reducers = combineReducers({
   calendar,
 });
 
-const store = createStore(reducers);
+const store = createStore(
+  reducers,
+  reduxDevtools && reduxDevtools()
+);
 
 export default store;
